922e9f841641866fcde957bd67ac031bf000606c
Rsambeek <rickvansambeek@gmail.com>
2018-01-17 16:04:14 +0100

    Servo code commented, added basic multithreading to main file

--- Code/Main.py
+++ Code/Main.py
@@ -1,12 +1,27 @@
-import Servo.py
-
+# import Servo.py   #Uncomment all servo related code when using on raspberry pi
+import threading
 
 height = 0
+stopThread = False
 
 def updateServo():
     global height
-    """some action to get water height in some value"""
-    if height < 50:
-        Servo.setServo(0)
-    else
-        Servo.setServo(0)
+    action = False  # Temp variable to replace the servo
+    for i in range(200):
+        if not stopThread:
+            # some action to get water height in some value
+            
+            if height < 50:
+                action = True# Temp variable to replace the servo
+                # Servo.setServo(0)
+            else:
+                action = False# Temp variable to replace the servo
+                # Servo.setServo(90)
+            print(threading.currentThread().getName(), action)
+        else:
+            break
+
+t = threading.Thread(name="Thread1",target=updateServo)    #setting up a thread
+t.start()
+
+stopThread = True   # How to stop the threads
--- Code/Servo.py
+++ Code/Servo.py
@@ -1,11 +1,11 @@
 import time
 import wiringpi
 
-servoPin1 = 18
+servoPin1 = 18  #Setup variables for pins for servo's
 servoPin2 = 19
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(servoPin1, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.wiringPiSetupGpio()    # Setup wiringpi to use gpio layout and not pin layout
+wiringpi.pinMode(servoPin1, wiringpi.GPIO.PWM_OUTPUT)   # Setup servo pins as Pulse Width Modulation pins
 wiringpi.pinMode(servoPin2, wiringpi.GPIO.PWM_OUTPUT)
 wiringpi.pwmSetClock(192)
 wiringpi.pwmSetRange(2000)
@@ -13,12 +13,12 @@ wiringpi.pwmSetRange(2000)
 delay_period = 0.01
 
 def setServo(angle):
-    if (angle > 180):
+    if (angle > 180):   # Define scope for the angles
         angle = 180
     elif (angle < 0):
         angle = 0
 
-    pulse1 = (angle/180*200)+50
+    pulse1 = (angle/180*200)+50     # Translate angle to pwm value
     pulse2 = 250-(angle/180*200)
-    wiringpi.pwmWrite(servoPin1, pulse1)
+    wiringpi.pwmWrite(servoPin1, pulse1)    # Change pwm to calculated value
     wiringpi.pwmWrite(servoPin2, pulse2)
