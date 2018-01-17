a143916b5bf6cf204ca5e71e5c3a8b6151eb4eaf
Rsambeek <rickvansambeek@gmail.com>
2018-01-17 12:32:33 +0100

    basic servo comtroll from value

---
+++ Code/Main.py
@@ -0,0 +1,12 @@
+import Servo.py
+
+
+height = 0
+
+def updateServo():
+    global height
+    """some action to get water height in some value"""
+    if height < 50:
+        Servo.setServo(0)
+    else
+        Servo.setServo(0)
