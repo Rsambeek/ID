88d7a5147e4c0d8cf91717741af95dce6add603e
Rsambeek <rickvansambeek@gmail.com>
2018-01-31 14:23:52 +0100

    pigpio might be amazing

--- Code/helper.py
+++ Code/helper.py
@@ -38,18 +38,16 @@ servoCheck1 = 29
 servoCheck2 = 31
 sensorTrigger = 32
 sensorEcho = 35
+pud = pigpio.PUD_DOWN   # if a high or a low should avtivate it
 
-wiringpi.wiringPiSetupGpio()    # Setup gpio to use pin layout
-wiringpi.pinMode(servoPin1, wiringpi.GPIO.PWM_OUTPUT) # Setup servo pins as output pins
-wiringpi.pinMode(servoPin2, wiringpi.GPIO.PWM_OUTPUT)
-wiringpi.pinMode(servoCheck1, 0)
-wiringpi.pinMode(servoCheck2, 0)
-wiringpi.pinMode(sensorTrigger, 1)
-wiringpi.pinMode(sensorEcho, 0)
+pi.set_mode(servoCheck1, pigpio.INPUT)
+pi.set_mode(servoCheck2, pigpio.INPUT)
+pi.set_mode(sensorEcho, pigpio.INPUT)
+pi.set_mode(sensorTrigger, pigpio.OUTPUT)
 
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
+pi.set_pull_up_down(servoCheck1, pud)
+pi.set_pull_up_down(servoCheck2, pud)
+pi.set_pull_up_down(sensorEcho, pud)
 
 
 def setServo(angle):
@@ -58,28 +56,28 @@ def setServo(angle):
     elif (angle < 0):
         angle = 0
 
-    pulse1 = (angle / 180)*200 + 50     # Translate angle to duty value
-    pulse2 = 250 - (angle / 180)*200
-    wiringpi.pwmWrite(servoPin1, pulse1)    # Change pwm to calculated value
-    wiringpi.pwmWrite(servoPin2, pulse2)
+    pulse1 = (angle / 180)*1800 + 550     # Translate angle to duty value
+    pulse2 = 2350 - (angle / 180)*1800
+    pi.set_servo_pulsewidth(servoPin1, pulse1)    # Change pwm to calculated value
+    pi.set_servo_pulsewidth(servoPin2, pulse2)
 
 def getServo():
     s1 = 0
     s2 = 0
     for i in range(50):
-        s1 += bin(wiringpi.digitalRead(servoCheck1))
-        s2 += bin(wiringpi.digitalRead(servoCheck2))
+        s1 += pi.read(servoCheck1)
+        s2 += pi.read(servoCheck2)
     return [s1,s2]
 
 def checkDistance(distance):
-    wiringpi.digitalWrite(TRIG, 1)
+    pi.write(TRIG, 1)
     time.sleep(0.00001)
-    wiringpi.digitalWrite(TRIG, 0)
+    pi.write(TRIG, 0)
 
-    while wiringpi.digitalRead(ECHO)==0:
+    while pi.read(ECHO)==0:
         pulse_start = time.time()
 
-    while wiringpi.digitalRead(ECHO)==1:
+    while pi.read(ECHO)==1:
         pulse_end = time.time()
 
     pulseTime = (pulse_end - (pulse_start/2))
